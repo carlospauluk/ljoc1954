990e91dfbd39278aed14b1541f1358ac331c9496
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2018-09-27 15:15:12 -0300

    Revert "Revert "pgrm""
    This reverts commit ccf9ee3d0a44db96aeb75a1a46d6646bdbb56e6b.

---
+++ catalog/language/portuguese-br/extension/payment/codemarket_pagarme.php
@@ -0,0 +1,6 @@
+<?php
+$_['text_title']          = 'Cartão de Crédito ou Boleto';
+$_['text_wait']           = "Por favor, aguarde alguns segundos";
+$_['button_confirm']      = 'Confirmar Pedido e Abrir Pagamento';
+$_['text_erro_pagamento'] = "<div class='alert alert-warning'>Problema com seu Pagamento, confirme de novo ou "
+    . "escolha outro método de Pagamento.<button type='button' class='close' data-dismiss='alert'>×</button></div>";
